82c8f05124b6ed60f9395134b0001fbf293a9a88
Nick Blackwell <nickblackwell82@gmail.com>
2015-02-14 14:02:44 -0800

    added username to geolive formatter. geolive specific

--- format/geolive.js
+++ format/geolive.js
@@ -6,7 +6,25 @@ function format(data){
 	
 	try{
 		if(data.url.indexOf('index.php?option=com_geolive&format=ajax')!==-1){
-			return {url:colors.blue('Geolive: ')+'ajax task: '+colors.blue(data.url.split('&task=')[1].split('&')[0])};
+			var p=data.url.indexOf('iam=');
+			var usr='';
+			if(p!==-1){
+				var e=data.url.indexOf('&',p+1);
+				if(e!==-1){
+					usr='('+data.url.substring(p+4,e)+') ';
+				}else{
+					usr='('+data.url.substring(p+4)+') ';
+				}
+			}
+			if(data.url.indexOf('&format=raw')!==-1){
+				
+				if(data.url.indexOf('&pluginView=')!==-1){
+					return {url:colors.blue('Geolive: '+usr)+'view: '+colors.blue((data.url.split('&plugin=')[1].split('&')[0])+'->'+(data.url.split('&pluginView=')[1].split('&')[0]))};
+				}else{	
+					return {url:colors.blue('Geolive: '+usr)+'view: '+colors.blue(data.url.split('&view=')[1].split('&')[0])};
+				}
+			}
+			return {url:colors.blue('Geolive: '+usr)+'ajax task: '+colors.blue(data.url.split('&task=')[1].split('&')[0])};
 		}
 	}catch(e){
 		console.log('url parser error');
