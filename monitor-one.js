79425ae0991d34cd9d1761bca1e977b828640af3
Nick Blackwell <nickblackwell82@gmail.com>
2015-03-09 10:38:59 -0700

    updated to monitor error log as well as access log, if named the same way.

--- monitor-one.js
+++ monitor-one.js
@@ -10,41 +10,83 @@ if(process.argv.length>=4){
 	label=process.argv[3];
 }
 
+var geocode=null;
+require('fs').exists(path,function(exists){
 
-require('fs').exists(path,function(exits){
 
-	if(exits){
-		
+
+
+	if(exists){
+		var error_log=false;
+		if(path.indexOf('-access_log')>=0){
+			var error_log=path.replace('-access_log','-error_log');
+		}
+
+
+		console.log('starting monitor: '+label=((error_log!==false)?' access, and error log':' access log'));
+
 		var lastlocation=false;
 		var lastip=null;
 		var colors = require("colors");
-		
-		require('./nasql.js').monitor(label , path).on('access',function(data){
-			
+
+		require('./nasql.js').monitor(label , path).on('log.access',function(data){
+
+			if(geocode===null){
+				geocode=require('./node-freegeoip.js');
+			}
 			var location=(function(obj){
 				if(obj===false)return false;
 				return obj.city+', '+obj.region_name+', '+obj.country_name;
 
-			})(require('./node-freegeoip.js').lookup(data.ip));
-			
+			})(geocode.lookup(data.ip));
+
 			if(lastlocation!==location||lastip!==data.ip){
 				console.log('');
 				console.log(colors.blue(data.ip+': '+(location!==false?', '+location:'')));
-				
-	
+
+
 				lastlocation=location;
 				lastip=data.ip;
 			}
-			
+
 			console.log(require('./log-format.js').format(data));
-		});	
+		}).on('error', function(data){
+			console.log(JSON.stringify(data));
+		});
+
+
+
+		require('fs').exists(error_log,function(exists){
+
+			
+			if(exists){
+
+				require('./nasql.js').monitor(label+' error_log' , error_log, 'error').on('log.error',function(data){
+	
+					console.log(JSON.stringify(data));
+	
+				}).on('error', function(data){
+					console.log(JSON.stringify(data));
+				});
+				
+			}
+
+		});
+
 
 	}else{
 		console.log('file:'+path+' does not exist');
 		process.exit(1);
 	}
 
+
+
+
 });
+
+
+
+
 
 
 
