c04022c25614b73a5ae6c195926f0b180032474a
Nick Blackwell <nickblackwell82@gmail.com>
2015-03-09 17:31:54 -0700

    connects to sock-monitor and prints entries acumulated for a client using a unique name to distiguish clients. this file also tries to start the sock-monitor if it is not available. but it must have read access on the log files or it will fail

---
+++ monitor-web.js
@@ -0,0 +1,94 @@
+
+if(process.argv.length<=3){
+	console.log('requires string:indentifier string:pathto-access_log, [label]');
+	process.exit(1);
+}
+
+var settings='./monitor_sock.json';
+var myname=process.argv[2];
+
+
+var path=process.argv[3];
+var label=path.split('/').pop();
+if(process.argv.length>=5){
+	label=process.argv[4];
+}
+
+
+var request=function(){
+	
+	require('fs').readFile(settings,function(err,data){
+		
+		var conf=JSON.parse(data);
+		//('./monitor_sock.json');
+		var socket = new require('net').Socket();
+		
+		socket.on('error',function(e){
+			console.error(e);
+		});
+		
+		socket.on('end',function(e){
+			//console.log('disconnected');
+		});
+		
+		socket.on('data',function(data){
+			
+			console.log(data);
+			socket.end();
+			
+		});
+		
+		socket.connect(conf.port,function(){
+			socket.setEncoding('utf8');
+			//console.log('connected');
+			socket.write('list updates '+myname);
+			
+		});
+		
+	});
+	
+}
+
+
+if(require('fs').exists(settings,function(exists){
+	
+	if(exists){
+		
+		//console.log('existed');
+		request();
+		
+		
+		
+	}else{
+		
+		
+		//start process.
+		
+		var out = require('fs').openSync('./monitor.log', 'a');
+		var err = require('fs').openSync('./monitor.log', 'a');
+		console.log('node '+__dirname+'/sock-monitor.js');
+		var child = require('child_process').spawn('node',[
+		                                                   __dirname+'/sock-monitor.js',
+		                                                   path,
+		                                                   label
+		                                                   ], {
+			detached: true,
+			stdio: [ 'ignore', out, err ]
+		});
+		
+
+		child.unref();
+		
+		setTimeout(function(){
+			//console.log('wait');
+			request();
+			}, 250);
+		
+	}
+	
+	
+	
+}));
+
+
+
