7343690271cda2da03274cca12549e680ceadbee
Nick Blackwell <nickblackwell82@gmail.com>
2015-04-07 17:57:41 -0700

    added second parser

--- format/time.js
+++ format/time.js
@@ -1,12 +1,23 @@
 module.exports = {
                 format: format
 }
+var colors = require("colors");
 function format(data){
 	
 	
 	if(data.time==undefined)return;
+	var u='ms';
+	var t=Math.round(data.time/100.0)/10.0;
 	
-	return {time:Math.round(data.time/100.0)/10.0+'ms'}
+	if(t>2000){
+		t=Math.round(t/100.0)/10.0;
+		u='s';
+		
+		if(t>5){
+			t=colors.red(t);
+		}
+	}
+	return {time:t+u}
 
 	
 }
