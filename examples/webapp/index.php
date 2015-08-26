ad2d672d60c69abb08ff6b9868b21acd9df2b5a7
nickolanack <nickblackwell82@gmail.com>
2015-08-26 11:14:43 -0700

    mobile width

--- examples/webapp/index.php
+++ examples/webapp/index.php
@@ -59,6 +59,8 @@ if (key_exists('get', $_GET)) {
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <style type="text/css">
 body {
 	margin: 30px;
